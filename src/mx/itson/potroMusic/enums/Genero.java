584360d88bfd2806557ebfc4bfcf54904eeaea63
Technologyland <Technologyland@10.232.11.63>
2025-03-13 09:56:17 -0700

    Se agregaron las clases de entidad acorde al modelo

---
+++ src/mx/itson/potroMusic/enums/Genero.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.potroMusic.enums;
+
+/**
+ *
+ * @author Technologyland
+ */
+public enum Genero {
+    
+    JPOP,
+    JROCK,
+    DANCE,
+    SALSA,
+    BANDA,
+    ELECTROSWING,
+    CLASSIC,
+    OTRO
+}
